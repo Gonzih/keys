23e2e8e63ad3ddafc41b51071b29d93a3ec33f61
Max Gonzih <gonzih@gmail.com>
2012-04-25 16:29:43 +0300

    proper install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
-curl http://keys.gonzih.org/laptop.asc  >> ~/.ssh/authorized_keys
-curl http://keys.gonzih.org/desktop.asc >> ~/.ssh/authorized_keys
+keys="laptop desktop"
+
+if which curl > /dev/null; then
+        for key in $keys
+        do
+                curl "http://keys.gonzih.org/$key.asc"  >> ~/.ssh/authorized_keys
+        done
+else
+        if which wget > /dev/null; then
+                for key in $keys
+                do
+                        wget "http://keys.gonzih.org/$key.asc" -O - >> ~/.ssh/authorized_keys
+                done
+        else
+                echo '#### curl or wget is required for installation ####'
+        fi
+fi
