3a97e5ec59fe98f82b0fe0e6aad513c6ee22993b
Max Gonzih <gonzih@gmail.com>
2013-11-28 19:51:35 +0300

    Add new rsa key, update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
-keys="laptop"
+keys="laptop_dsa.pub lapbap_rsa.pub"
 
 if which curl > /dev/null; then
         for key in $keys
         do
-                curl "http://keys.gonzih.org/$key.asc"  >> ~/.ssh/authorized_keys
+                curl "http://keys.gonzih.org/$key"  >> ~/.ssh/authorized_keys
         done
 else
         if which wget > /dev/null; then
                 for key in $keys
                 do
-                        wget "http://keys.gonzih.org/$key.asc" -O - >> ~/.ssh/authorized_keys
+                        wget "http://keys.gonzih.org/$key" -O - >> ~/.ssh/authorized_keys
                 done
         else
                 echo '#### curl or wget is required for installation ####'
