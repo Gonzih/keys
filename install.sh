ad6ba14ccd9c5ef6719c05ea2dfa75a989aa8b01
Max Gonzih <gonzih@gmail.com>
2013-11-28 20:05:56 +0300

    Fuck...

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
-keys="laptop_dsa.pub lapbap_rsa.pub"
+keys="laptop_dsa.pub laptop_rsa.pub"
 
 if which curl > /dev/null; then
         for key in $keys
         do
-                curl "http://keys.gonzih.org/$key"  >> ~/.ssh/authorized_keys
+                curl "http://keys.gonzih.me/$key"  >> ~/.ssh/authorized_keys
         done
 else
         if which wget > /dev/null; then
                 for key in $keys
                 do
-                        wget "http://keys.gonzih.org/$key" -O - >> ~/.ssh/authorized_keys
+                        wget "http://keys.gonzih.me/$key" -O - >> ~/.ssh/authorized_keys
                 done
         else
                 echo '#### curl or wget is required for installation ####'
